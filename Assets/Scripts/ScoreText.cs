e8f929f14db24a795f6ce8979f7c318a387e74d9
Marcin Drelewski <drelewski@gmail.com>
2020-10-30 23:10:27 +0100

    first commit

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    private int score = 0;
+    private TextMeshProUGUI text;
+    private void Start()
+    {
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+        GameManager.OnCubeSpawned += GameManager_OnCubeSpawned;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnCubeSpawned -= GameManager_OnCubeSpawned;
+    }
+
+    private void GameManager_OnCubeSpawned()
+    {
+        score += 10;
+        text.text = "Score: " + score;
+    }
+}
